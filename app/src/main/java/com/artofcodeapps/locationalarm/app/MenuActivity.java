93ed4a159b393cc197cb02ed5058c451d0ba0872
Pete <petri.mkinen1@gmail.com>
2014-04-08 19:31:38 +0300

    mapactivity created

--- app/src/main/java/com/artofcodeapps/locationalarm/app/MenuActivity.java
+++ app/src/main/java/com/artofcodeapps/locationalarm/app/MenuActivity.java
@@ -1,23 +1,106 @@
 package com.artofcodeapps.locationalarm.app;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.List;
 
 
 public class MenuActivity extends ActionBarActivity {
+    private LocationManager manager;
+    String name = "testi";
+
+    // Google Map
+    private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        manager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+        initializeMap();
     }
 
+    public String getName(){
+        return name;
+    }
+
+    /**
+     * function to load map. If map is not created it will create it for you
+     * */
+    private void initializeMap() {
+        if (googleMap == null) {
+            googleMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(
+                    R.id.map)).getMap();
+            googleMap.setMyLocationEnabled(true);
+            googleMap.getUiSettings().setZoomControlsEnabled(false);
+            googleMap.getUiSettings().setRotateGesturesEnabled(false);
+            Location loc = getLocation();
+
+            CameraPosition pos = new CameraPosition.Builder().target(new LatLng(loc.getLatitude(), loc.getLongitude())).zoom(15).build();
+            googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(pos));
+            // check if map is created successfully or not
+            if (googleMap == null) {
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
+
+    private Location getLocation(){
+        int minTime = 0;
+        List<String> matchingProviders = manager.getAllProviders();
+        String prov = matchingProviders.get(0);
+        Location loc = manager.getLastKnownLocation(prov);
+        float bestAccuracy = loc.getAccuracy();
+        Location bestResult = loc;
+        long bestTime = loc.getTime();
+
+        for (String provider: matchingProviders) {
+            Location location = manager.getLastKnownLocation(provider);
+
+            if (location != null) {
+                float accuracy = location.getAccuracy();
+                long time = location.getTime();
+
+                if ((time > minTime && accuracy < bestAccuracy)) {
+                    bestResult = location;
+                    bestAccuracy = accuracy;
+                    bestTime = time;
+                }
+                else if (time < minTime &&
+                        bestAccuracy == Float.MAX_VALUE && time > bestTime){
+                    bestResult = location;
+                    bestTime = time;
+                }
+            }
+        }
+        return bestResult;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initializeMap();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
@@ -34,5 +117,6 @@ public class MenuActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
 
 }
